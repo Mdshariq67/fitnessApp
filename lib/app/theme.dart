82be68a052267d52f36e254f8b0e4fed0253f534
Md Sharique Minhaz <mdshariqueminhaz@gmail.com>
2024-09-20 02:04:29 +0530

    fitnessapp first commit

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -4,12 +4,10 @@ class AppThemes {
   static final lightTheme = ThemeData(
     primarySwatch: Colors.blue,
     brightness: Brightness.light,
-    // Customize light theme here
   );
 
   static final darkTheme = ThemeData(
     primarySwatch: Colors.blue,
     brightness: Brightness.dark,
-    // Customize dark theme here
   );
 }
